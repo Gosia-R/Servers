47f114fe98d3cdf61dca8dad98adc594bc01d20f
Gosia-R <58257515+Gosia-R@users.noreply.github.com>
2019-11-27 13:53:29 +0100

    Update servers_test.py

--- servers_test.py
+++ servers_test.py
@@ -23,6 +23,22 @@ class ClientTest(unittest.TestCase):
             server = server_type(products)
             client = Client(server)
             self.assertEqual(5, client.get_total_price(2))
+      
+    #testy ale nie wiem jeszcze czy działają
+    def test_total_price_for_no_match_found_execution(self):
+        products = [Product('PP234', 2), Product('PP235', 3)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(0, client.get_total_price(3))
+            
+     def test_total_price_for_too_many_entries_execution(self):
+        products = [Product('PP234', 2), Product('PP235', 3), Product('PP236', 4), Product('PP237', 1), Product('PP238', 3)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            #nie wiem tu do czego porównać
+            self.assertEqual(5, client.get_total_price(2))
 
 
 if __name__ == '__main__':
